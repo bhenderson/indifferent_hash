e204cd1413e54b602dc113fce6751f237c1146e2
Brian Henderson <henderson.bj@gmail.com>
2017-01-04 12:16:49 -0800

    Rename test variable.

--- test/test_indifferent_hash.rb
+++ test/test_indifferent_hash.rb
@@ -3,24 +3,24 @@ require "indifferent_hash"
 
 class TestIndifferentHash < MiniTest::Test
   def setup
-    @ahash = IndifferentHash["foo" => "bar"]
+    @hash = IndifferentHash["foo" => "bar"]
   end
 
   def test_class_create_from_hash
-    @ahash = IndifferentHash[:foo => {:bar => :baz, :arr => [{:taz => 1}]}]
-    assert_kind_of IndifferentHash, @ahash["foo"]
-    assert_kind_of IndifferentHash, @ahash["foo"]["arr"][0]
+    @hash = IndifferentHash[:foo => {:bar => :baz, :arr => [{:taz => 1}]}]
+    assert_kind_of IndifferentHash, @hash["foo"]
+    assert_kind_of IndifferentHash, @hash["foo"]["arr"][0]
   end
 
   def test_class_create_from_tuple
-    @ahash = IndifferentHash[:foo, :bar, :baz, :taz]
-    assert_kind_of IndifferentHash, @ahash
-    assert_equal Hash["foo", :bar, "baz", :taz], @ahash.to_h
+    @hash = IndifferentHash[:foo, :bar, :baz, :taz]
+    assert_kind_of IndifferentHash, @hash
+    assert_equal Hash["foo", :bar, "baz", :taz], @hash.to_h
   end
 
   def test_class_create_from_array
-    @ahash = IndifferentHash[ [[:a], [:b, 2]] ]
-    assert_equal({"a" => nil, "b" => 2}, @ahash)
+    @hash = IndifferentHash[ [[:a], [:b, 2]] ]
+    assert_equal({"a" => nil, "b" => 2}, @hash)
   end
 
   def test_class_create_fails_with_array_of_non_tuples
@@ -33,105 +33,105 @@ class TestIndifferentHash < MiniTest::Test
   end
 
   def test_aref_with_string
-    assert_equal "bar", @ahash["foo"]
+    assert_equal "bar", @hash["foo"]
   end
 
   def test_aref_with_symbol
-    assert_equal "bar", @ahash[:foo]
+    assert_equal "bar", @hash[:foo]
   end
 
   def test_store_with_symbol
-    @ahash[:bar] = "baz"
-    assert_equal "baz", @ahash["bar"]
+    @hash[:bar] = "baz"
+    assert_equal "baz", @hash["bar"]
   end
 
   def test_equal_equal
-    exp, act = Hash[:foo => "bar"], @ahash
+    exp, act = Hash[:foo => "bar"], @hash
     msg = message(msg, E) { diff exp, act }
     assert act == exp, msg # assert_equal is defined as exp == act, but I'm testing act.==()
   end
 
   def test_values_at
-    @ahash["bar"] = "baz"
-    assert_equal ["bar", "baz"], @ahash.values_at(:foo, :bar)
+    @hash["bar"] = "baz"
+    assert_equal ["bar", "baz"], @hash.values_at(:foo, :bar)
   end
 
   def test_delete
-    assert_equal "bar", @ahash.delete(:foo)
-    assert_equal [], @ahash.keys
+    assert_equal "bar", @hash.delete(:foo)
+    assert_equal [], @hash.keys
   end
 
   def test_invert
-    @ahash[:bar] = :baz
-    @inverted = @ahash.invert
+    @hash[:bar] = :baz
+    @inverted = @hash.invert
     assert_equal Hash["baz" => "bar", "bar" => "foo"], @inverted
     assert_instance_of IndifferentHash, @inverted
   end
 
   def test_key_eh
-    assert_operator @ahash, :include?, :foo
-    assert_operator @ahash, :member?, :foo
-    assert_operator @ahash, :has_key?, :foo
-    assert_operator @ahash, :key?, :foo
+    assert_operator @hash, :include?, :foo
+    assert_operator @hash, :member?, :foo
+    assert_operator @hash, :has_key?, :foo
+    assert_operator @hash, :key?, :foo
   end
 
   def test_dig
-    @ahash[:foo] = IndifferentHash["bar" => "baz"]
-    assert_equal "baz", @ahash.dig(:foo, :bar)
+    @hash[:foo] = IndifferentHash["bar" => "baz"]
+    assert_equal "baz", @hash.dig(:foo, :bar)
   end
 
   def test_update
-    @ahash.update :bar => :baz
-    assert_equal :baz, @ahash["bar"]
+    @hash.update :bar => :baz
+    assert_equal :baz, @hash["bar"]
 
-    @ahash.update(:foo => 3) { |*arg| arg }
-    assert_equal ["foo", "bar", 3], @ahash[:foo]
+    @hash.update(:foo => 3) { |*arg| arg }
+    assert_equal ["foo", "bar", 3], @hash[:foo]
   end
 
   def test_store_recursive
-    @ahash[:bar] = {:baz => :taz}
-    assert_kind_of IndifferentHash, @ahash[:bar]
+    @hash[:bar] = {:baz => :taz}
+    assert_kind_of IndifferentHash, @hash[:bar]
   end
 
   def test_default
     # The 2.x docs as well as ActiveSupport say that default(key) should
     # return like fetch(key, default) but in my testing, no ruby version ever
     # did this.
-    @ahash.default = 3
-    assert_equal 3, @ahash.default("foo")
+    @hash.default = 3
+    assert_equal 3, @hash.default("foo")
   end
 
   def test_convert_subclass
     @klass = Class.new(IndifferentHash)
-    @ahash = @klass[:foo => {:bar => :baz}]
-    assert_kind_of @klass, @ahash["foo"]
+    @hash = @klass[:foo => {:bar => :baz}]
+    assert_kind_of @klass, @hash["foo"]
   end
 
   def test_fetch
-    assert_equal "bar", @ahash.fetch(:foo)
-    assert_equal "bar", @ahash.fetch(:bar){|key| key}
+    assert_equal "bar", @hash.fetch(:foo)
+    assert_equal "bar", @hash.fetch(:bar){|key| key}
   end
 
   def test_merge
-    @newhash = @ahash.merge("bar" => "baz")
-    refute @ahash.key?("bar")
+    @newhash = @hash.merge("bar" => "baz")
+    refute @hash.key?("bar")
     assert_kind_of IndifferentHash, @newhash
     assert_equal "baz", @newhash[:bar]
   end
 
   def test_replace
-    @ahash.replace(:bar => "baz")
-    assert_equal ["bar"], @ahash.keys
+    @hash.replace(:bar => "baz")
+    assert_equal ["bar"], @hash.keys
   end
 
   def test_to_h
-    @ahash[:foo] = {:bar => :baz}
-    assert_kind_of Hash, @ahash.to_h
-    assert_kind_of IndifferentHash, @ahash["foo"]
-    assert_kind_of Hash, @ahash.to_h["foo"]
+    @hash[:foo] = {:bar => :baz}
+    assert_kind_of Hash, @hash.to_h
+    assert_kind_of IndifferentHash, @hash["foo"]
+    assert_kind_of Hash, @hash.to_h["foo"]
   end
 
   def test_assoc
-    assert_equal ["foo", "bar"], @ahash.assoc(:foo)
+    assert_equal ["foo", "bar"], @hash.assoc(:foo)
   end
 end
